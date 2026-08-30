59051fb77bc3c5d532a78f43afc268883983f21c
savaliyayashvi49-crypto <savaliyayashvi49@gmail.com>
2026-08-30 21:16:43 +0530

    Add files via upload

---
+++ project-2 logic box.py
@@ -0,0 +1,34 @@
+print("welcome")
+print("="*40)
+print("mind pattern ditective")
+print("="*40)
+print("LEVEL-01 Number DNA")
+print("find the missing numbers")
+print("1,2,6,21,110....?")
+answer=input("your answer:")
+if answer=="771":
+    print("correct")
+else:
+    print("wrong! right answer is 771")
+print("LEVEL-02")
+print("find the missing numbers")
+numbers=[3,6,9,12,15,18]
+for i in range(len(numbers)):
+    print(numbers[i],end="")
+print("what comes next?")
+answer=input("your answer:" )
+if answer=="21":
+   print("correct")
+else:
+    print("wrong! right answer is 21")
+print("LEVEL-03 HIDDEN RULE")
+print("a=1")
+print("b=2")
+print("c=3")
+print("Then=LQX.......?")
+answer=input("your answer:")
+if answer=="24":
+   print("correct")
+else:
+    print("wrong!right answer is 24")
+print("Thank you")
