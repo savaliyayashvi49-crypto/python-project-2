e5ed74d3bd8abf4afd871c143bcb85088d202b45
savaliyayashvi49-crypto <savaliyayashvi49@gmail.com>
2026-08-31 22:03:29 +0530

    Implement diamond pattern in pattern logic program
    Added diamond pattern logic to the existing pattern logic program.

--- project-2 logic box.py
+++ project-2 logic box.py
@@ -44,4 +44,20 @@ for i in range(1, n + 1):
 
     print()
 
+
+print("DIMOND PATTERN")
+rows=int(input("Enter number of rows:"))
+for i in range(1 , rows+1):
+    for j in range(rows-i):
+        print(" ",end=" ")
+    for j in range(2*i-1):
+       print("*",end=" ")
+    print()
+for i in range(rows-1,0,-1):
+    for j in range(rows-i):
+        print(" ",end=" ")
+    for j in range(2*i-1):
+        print("*",end=" ")
+    print()
+
     
